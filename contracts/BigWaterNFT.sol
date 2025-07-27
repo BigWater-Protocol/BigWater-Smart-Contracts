c6a763ae7d45e3e0e6c5203da7097ebc701d2417
vbhattaccmu <53374818+vbhattaccmu@users.noreply.github.com>
2025-07-27 11:55:36 +0530

    deployment step

--- contracts/BigWaterNFT.sol
+++ contracts/BigWaterNFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable2Step.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.1/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.1/contracts/access/Ownable2Step.sol";
 
 /// @title BigWater Device NFT Contract
 /// @notice ERC721 contract for minting NFTs representing registered devices
