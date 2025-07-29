27d0165a61e969eadafbbcfe0829b2d29b307979
rukimedo1992 <n7092180@gmail.com>
2025-07-29 16:01:06 +0530

    Revert "Update BigWaterToken.sol"
    This reverts commit a2b0276cb9104dabc4eced8f68fcc8058f4303ed.

--- contracts/BigWaterToken.sol
+++ contracts/BigWaterToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-3pragma solidity =0.8.23;
+3pragma solidity =0.8.26;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.1/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.1/contracts/access/Ownable2Step.sol";
